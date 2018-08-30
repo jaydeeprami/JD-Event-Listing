1fef0752425fed30c9454a6857d0e2173a9b843d
Jaydeep <jaydeep.ramii@gmail.com>
2018-08-30 21:42:40 +0530

    register event custom post type

--- includes/admin/class-jd-event-listing-admin.php
+++ includes/admin/class-jd-event-listing-admin.php
@@ -16,6 +16,9 @@ class JD_Event_Listing_Admin {
 		// Enqueue Script and Style for Admin.
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+		// Register Event post type.
+		add_action( 'init', array( $this, 'register_jd_events' ), 10 );
 	}
 
 	/**
@@ -47,4 +50,48 @@ class JD_Event_Listing_Admin {
 		wp_enqueue_script( JD_EVENT_LISTING_SLUG );
 
 	}
+
+	/**
+	 * Register Custom Post type for Event.
+	 *
+	 * @since 1.0.0
+	 */
+	public function register_jd_events() {
+		$labels = array(
+			'name'               => _x( 'Events', 'event type general name', 'jd-event-list' ),
+			'singular_name'      => _x( 'Event', 'event type singular name', 'jd-event-list' ),
+			'menu_name'          => _x( 'Events', 'admin menu', 'jd-event-list' ),
+			'name_admin_bar'     => _x( 'Event', 'add new on admin bar', 'jd-event-list' ),
+			'add_new'            => _x( 'Add New', 'event', 'jd-event-list' ),
+			'add_new_item'       => __( 'Add New Event', 'jd-event-list' ),
+			'new_item'           => __( 'New Event', 'jd-event-list' ),
+			'edit_item'          => __( 'Edit Event', 'jd-event-list' ),
+			'view_item'          => __( 'View Event', 'jd-event-list' ),
+			'all_items'          => __( 'All Events', 'jd-event-list' ),
+			'search_items'       => __( 'Search Events', 'jd-event-list' ),
+			'parent_item_colon'  => __( 'Parent Events:', 'jd-event-list' ),
+			'not_found'          => __( 'No events found.', 'jd-event-list' ),
+			'not_found_in_trash' => __( 'No events found in Trash.', 'jd-event-list' ),
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'description'        => __( 'Event Listing', 'jd-event-list' ),
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'events' ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 20,
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'custom-fields' ),
+		);
+
+		register_post_type( 'jd_event', $args );
+	}
 }
+
+new JD_Event_Listing_Admin();
